60abec1a17d7b4d12276e7aede73b4143f1dea72
Vien Bui <buivien314@gmail.com>
2026-05-29 06:27:48 +0700

    feat: all 5 TCs rewrite - day 2

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,60 +1,67 @@
-import {expect, test} from '@playwright/test';
- 
-test('001 - Login success', async ({page}) => {
-    await page.goto(`https://www.saucedemo.com/`);
-    await page.locator("#user-name").fill('standard_user');
-    await page.locator("#password").fill('secret_sauce');
+import {expect, test} from '@playwright/test'
+
+
+test ("TC01 — Login successfully with valid credentials", async ({page}) => {
+    await page.goto('https://www.saucedemo.com')
+    await page.locator("#user-name").fill("standard_user")
+    await page.locator("#password").fill("secret_sauce")
     await page.locator("#login-button").click()
+
     await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html")
     await expect(page.locator(".inventory_list")).toBeVisible()
+    await expect(page.locator(".app_logo")).toContainText("Swag Labs")
+
 })
 
- test('TC02 — Login fails with wrong password', async ({page}) => {
-    await page.goto(`https://www.saucedemo.com`)
-    await page.locator("#user-name").fill('standard_user')
-    await page.locator("#password").fill('wrong_password');
+test ("TC02 — Login fails with wrong password", async ({page}) => {
+    await page.goto('https://www.saucedemo.com')
+    await page.locator("#user-name").fill("standard_user")
+    await page.locator("#password").fill("wrong_password")
     await page.locator("#login-button").click()
-    const errorMessage = page.locator("[data-test='error']")
-    await expect (errorMessage).toBeVisible()
-    await expect(errorMessage).toHaveText("Epic sadface: Username and password do not match any user in this service")   
-    await expect(page).not.toHaveURL('/inventory')
- })
 
- test('TC03 — Login fails with empty credentials', async ({page}) => {
-    await page.goto(`https://www.saucedemo.com`)
-    await page.locator("#login-button").click()
-    const errorMessage = page.locator("[data-test='error']")
-    await expect (errorMessage).toBeVisible()
-    await expect(errorMessage).toHaveText("Epic sadface: Username is required")
-    await expect(page).not.toHaveURL('/inventory')
- })
+    await expect(page).not.toHaveURL("/inventory")
+    await expect(page.locator("[data-test='error']")).toBeVisible()
+    await expect(page.locator("[data-test='error']")).toHaveText("Epic sadface: Username and password do not match any user in this service")
+    
 
- test("TC04 — Add a product to cart", async ({page}) => {
-    await page.goto(`https://www.saucedemo.com/`);
-    await page.locator("#user-name").fill('standard_user');
-    await page.locator("#password").fill('secret_sauce');
+})
+test ("TC03 — Login fails with empty fields", async ({page}) => {
+    await page.goto('https://www.saucedemo.com')
+    
     await page.locator("#login-button").click()
-    await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html")
-    await expect(page.locator(".inventory_list")).toBeVisible()
+
+    await expect(page).not.toHaveURL("https://www.saucedemo.com/inventory.html")
+    await expect(page.locator("[data-test='error']")).toBeVisible()
+    await expect(page.locator("[data-test='error']")).toHaveText("Epic sadface: Username is required")
+
+})
+
+test("TC04 — Add a product to cart", async({page}) => {
+    await page.goto('https://www.saucedemo.com')
+    await page.locator("#user-name").fill("standard_user")
+    await page.locator("#password").fill("secret_sauce")
+    await page.locator("#login-button").click()
+
     await page.locator(".inventory_item:first-child button").click()
+
+    await expect(page.locator(".inventory_item:first-child button")).toHaveText("Remove")
+    await expect(page.locator(".shopping_cart_badge")).toBeVisible()
     await expect(page.locator(".shopping_cart_badge")).toHaveText("1")
-   await expect(page.locator(".inventory_item:first-child button")).toHaveText("Remove")
+})
 
- })
-
- test("TC05-Logout", async ({page}) => {
-   await page.goto(`https://www.saucedemo.com/`);
-    await page.locator("#user-name").fill('standard_user');
-    await page.locator("#password").fill('secret_sauce');
+test("TC05 — Logout successfully", async ({page})=> {
+    await page.goto('https://www.saucedemo.com')
+    await page.locator("#user-name").fill("standard_user")
+    await page.locator("#password").fill("secret_sauce")
     await page.locator("#login-button").click()
-    await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html")
-    await expect(page.locator(".inventory_list")).toBeVisible()
+
     await page.locator("#react-burger-menu-btn").click()
     await page.locator("#logout_sidebar_link").click()
-    await expect(page).toHaveURL('https://www.saucedemo.com/')
+
+    await expect(page).toHaveURL("https://www.saucedemo.com")
     await expect(page.locator("#user-name")).toBeVisible()
 
-    await page.goto(`https://www.saucedemo.com/inventory.html`);
-    await expect(page).not.toHaveURL('/inventory')
+    await page.goto('https://www.saucedemo.com/inventory.html')
+    await expect(page).toHaveURL("https://www.saucedemo.com")
 
- })
+})
