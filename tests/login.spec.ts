4cc810e6971aef877cb61e7ae313286a7aa483a2
Vien Bui <buivien314@gmail.com>
2026-05-28 20:57:54 +0700

    feat: TC03 login fails empty credentials - day 1

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -66,3 +66,12 @@ AC (must all pass):
     await expect(errorMessage).toHaveText("Epic sadface: Username and password do not match any user in this service")   
     await expect(page).not.toHaveURL('/inventory')
  })
+
+ test('TC03 — Login fails with empty credentials', async ({page}) => {
+    await page.goto(`https://www.saucedemo.com`)
+    await page.locator("#login-button").click()
+    const errorMessage = page.locator("[data-test='error']")
+    await expect (errorMessage).toBeVisible()
+    await expect(errorMessage).toHaveText("Epic sadface: Username is required")
+    await expect(page).not.toHaveURL('/inventory')
+ })
