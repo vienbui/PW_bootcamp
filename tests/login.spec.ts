af604fd80b0e0d788717b7768fa845e9240782e2
Vien Bui <buivien314@gmail.com>
2026-05-28 21:44:35 +0700

    feat: all 5 TCs clean - day 1

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,6 +1,4 @@
-import {Page,expect, test} from '@playwright/test';
-
-//This test verifies that a user with valid credentials is successfully redirected to the inventory page after login. It confirms the expected URL and that the product list is visible.
+import {expect, test} from '@playwright/test';
  
 test('001 - Login success', async ({page}) => {
     await page.goto(`https://www.saucedemo.com/`);
@@ -10,9 +8,6 @@ test('001 - Login success', async ({page}) => {
     await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html")
     await expect(page.locator(".inventory_list")).toBeVisible()
 })
-
-
-//"This test verifies that a user with an incorrect password cannot log in, remains on the login page, and sees a specific error message."
 
  test('TC02 — Login fails with wrong password', async ({page}) => {
     await page.goto(`https://www.saucedemo.com`)
@@ -61,7 +56,5 @@ test('001 - Login success', async ({page}) => {
 
     await page.goto(`https://www.saucedemo.com/inventory.html`);
     await expect(page).not.toHaveURL('/inventory')
-    
-
 
  })
